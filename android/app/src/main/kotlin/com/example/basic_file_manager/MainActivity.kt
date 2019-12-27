a86c320a3bb15b80f2851ffe4b10f7a775555c12
eagle6789 <me.dev6789@gmail.com>
2019-12-27 02:20:26 +0200

    ?

--- android/app/src/main/kotlin/com/example/basic_file_manager/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/basic_file_manager/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.basic_file_manager
 
 import android.os.Bundle
-
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+
+class MainActivity : FlutterActivity() {
+    val CHANNEL = "samples.flutter.dev/storage"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+    }
+
 }
+
+
